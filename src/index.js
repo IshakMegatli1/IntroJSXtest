4497007f92fa4dca418e43a33441c03c1011145c
Ishak Megatli <49136256+IshakMegatli1@users.noreply.github.com>
2025-01-03 20:30:24 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+/*var React = require("react");
+var ReactDOM = require("react-dom");*/
+
+ReactDOM.render(
+  <div>
+    <h1>Hello world!</h1>
+    <p>this is a paragraph.</p>
+  </div>,
+
+  document.getElementById("root")
+); //(WHAT TO SHOW, WHERE TO SHOW IT)
+
+/*var h1 = document.createElement("h1");
+h1.innerHTML = "Hello za World";
+document.getElementById("root").appendChild(h1);*/
